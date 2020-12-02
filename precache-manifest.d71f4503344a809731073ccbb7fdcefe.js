c826126b72341c755aaf035a046e40fc04260dc2
Vita  Kozlovska <vitakozlovska5@gmail.com>
2020-12-02 21:51:46 +0200

    Updates

---
+++ precache-manifest.d71f4503344a809731073ccbb7fdcefe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0ae050207df2ad95e5b077135393b7",
+    "url": "/OrderToDo/index.html"
+  },
+  {
+    "revision": "203de4862cdc429a9788",
+    "url": "/OrderToDo/static/css/main.94cab8ad.chunk.css"
+  },
+  {
+    "revision": "b8a9bd04eb74b3a32dc4",
+    "url": "/OrderToDo/static/js/2.6334aa50.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/OrderToDo/static/js/2.6334aa50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "203de4862cdc429a9788",
+    "url": "/OrderToDo/static/js/main.d2a7e42c.chunk.js"
+  },
+  {
+    "revision": "c0506958cd59ece62f31",
+    "url": "/OrderToDo/static/js/runtime-main.8e25eade.js"
+  },
+  {
+    "revision": "1cfd1aa3b4fe4bba10c132acea9985af",
+    "url": "/OrderToDo/static/media/Roboto-Regular.1cfd1aa3.woff"
+  },
+  {
+    "revision": "2222f1fd23aa2c08af158311d680ac4a",
+    "url": "/OrderToDo/static/media/Roboto-Regular.2222f1fd.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.281e51dd2c371412c5d7b6df49694ea8.js"
+  "/OrderToDo/precache-manifest.d71f4503344a809731073ccbb7fdcefe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/OrderToDo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
